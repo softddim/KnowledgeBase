a25b7cedbcaf121a7e6049cfca74850a56dc3647
yudong <1223277987@qq.com>
2014-11-03 21:25:19 +0800

    添加src包

---
+++ src/baseCreateSubSystem/Test.java
@@ -0,0 +1,11 @@
+package baseCreateSubSystem;
+/**
+ * @project: KnowledgeBase, the task of this project is to create a konwledge base 
+ *           and provide serach services.
+ * @author: yudong
+ * @date: 2014-11-3 обнГ9:24:17
+ * @function: 
+ */
+public class Test {
+
+}
---
+++ src/baseSearchSubSystem/Test.java
@@ -0,0 +1,11 @@
+package baseSearchSubSystem;
+/**
+ * @project: KnowledgeBase, the task of this project is to create a konwledge base 
+ *           and provide serach services.
+ * @author: yudong
+ * @date: 2014-11-3 обнГ9:24:48
+ * @function: 
+ */
+public class Test {
+
+}
